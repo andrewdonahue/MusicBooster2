c7f9b85d767ef7f4b1afc8ecfc50e4c4defbbe6f
andrewdonahue <donahue00@gmail.com>
2019-04-16 13:52:20 -0400

    Initial Commit

---
+++ MusicBooster2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MusicBooster2
+//
+//  Created by  on 4/16/19.
+//  Copyright © 2019 Donahue_Apps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MusicBooster2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MusicBooster2
+//
+//  Created by  on 4/16/19.
+//  Copyright © 2019 Donahue_Apps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
